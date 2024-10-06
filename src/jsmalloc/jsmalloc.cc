c2b70b2f2358157252e6efea1cffac17e541f040
jackson <jtstogel@berkeley.edu>
2024-10-06 12:45:51 -0700

    fix log copy

--- src/jsmalloc/jsmalloc.cc
+++ src/jsmalloc/jsmalloc.cc
@@ -132,9 +132,10 @@ void* realloc(void* ptr, size_t size) {
     }
   }
 
-  DEBUG_LOG_IF(!heap_globals->large_block_region_->Contains(ptr) &&
+  DEBUG_LOG_IF(ptr != nullptr &&
+                   !heap_globals->large_block_region_->Contains(ptr) &&
                    !heap_globals->small_block_region_->Contains(ptr),
-               "free() called with unmapped ptr");
+               "ERROR: realloc(%p, %zu) called with unmapped ptr\n", ptr);
 
   return default_realloc(ptr, size);
 }
@@ -149,8 +150,9 @@ void free(void* ptr, size_t, size_t) {
     return;
   }
 
-  DEBUG_LOG_IF(!heap_globals->large_block_region_->Contains(ptr),
-               "free() called with unmapped ptr");
+  DEBUG_LOG_IF(!heap_globals->large_block_region_->Contains(ptr) &&
+                   !heap_globals->small_block_region_->Contains(ptr),
+               "free(%p) called with unmapped ptr\n", ptr);
 
   heap_globals->large_block_allocator_.Free(ptr);
 }
